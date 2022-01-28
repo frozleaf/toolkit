b272c194bb2f8a58899006b11d99308aa432cc8e
frozleaf <frozleaf@qq.com>
2022-01-28 14:14:59 +0800

    Data

---
+++ tools/RolanAppDownloader样例文件/init_app.bat
@@ -0,0 +1,10 @@
+@ECHO OFF & CD /D %~DP0 & TITLE °²×°
+>NUL 2>&1 REG.exe query "HKU\S-1-5-19" || (
+    ECHO SET UAC = CreateObject^("Shell.Application"^) > "%TEMP%\Getadmin.vbs"
+    ECHO UAC.ShellExecute "%~f0", "%1", "", "runas", 1 >> "%TEMP%\Getadmin.vbs"
+    "%TEMP%\Getadmin.vbs"
+    DEL /f /q "%TEMP%\Getadmin.vbs" 2>NUL
+    Exit /b
+)
+
+xcopy/e/y "%~DP0Fork\gitInstance" "%LocalAppData%\Fork\gitInstance\"
